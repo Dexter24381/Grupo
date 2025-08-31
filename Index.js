5ecdaa02611eed0a543bcd6e3285672372ebe37a
Dexter24381 <Dexter24381@gmail.com>
2025-08-31 21:54:33 +0200

    Add files via upload

---
+++ Index.js
@@ -0,0 +1,10 @@
+ <script>
+        // Función para redirigir a los portafolios
+        function redirectToPortfolio(url) {
+            if (url) {
+                window.open(url, '_blank');
+            }
+        }
+    </script>
+</body>
+</html>
